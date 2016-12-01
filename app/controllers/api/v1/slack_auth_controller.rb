7e1e158815b646d4b73fd5ae26ee89456d7dbd35
Matthew Perkins <mfperkins@gmail.com>
2016-12-01 17:30:04 +0000

    updating gemfile

--- app/controllers/api/v1/slack_auth_controller.rb
+++ app/controllers/api/v1/slack_auth_controller.rb
@@ -32,7 +32,7 @@ class Api::V1::SlackAuthController < ApplicationController
     code = params[:code]
     client_id = ENV.fetch('SLACK_CLIENT_ID')
     client_secret = ENV.fetch('SLACK_CLIENT_SECRET')
-    slack_reponse = HTTParty.get("https://slack.com/api/oauth.access&client_id=" + client_id + "&client_secret=" + client_secret + "&code=" + code)
+    slack_response = HTTParty.get("https://slack.com/api/oauth.access&client_id=" + client_id + "&client_secret=" + client_secret + "&code=" + code)
     puts slack_response
   end
 
