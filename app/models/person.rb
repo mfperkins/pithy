543793dd20d0432b235a11455a35ba52d1e51dd4
Matthew Perkins <mfperkins@gmail.com>
2016-12-08 09:18:55 +0000

    adding in validations for quotes and people

--- app/models/person.rb
+++ app/models/person.rb
@@ -3,9 +3,15 @@ class Person < ApplicationRecord
   extend FriendlyId
   friendly_id :nickname, use: :slugged
 
-  has_attached_file :image, :styles => { :large => "500x500",:medium => "300x300>", :thumb => "75x75>" }, :default_url => "default_image.jpg"
   has_many :quotes, dependent: :destroy
+  has_attached_file :image, :styles => { :large => "500x500",:medium => "300x300>", :thumb => "75x75>" }, :default_url => "default_image.jpg"
 
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :nickname, presence: true
+  validates :first_name, uniqueness: true
+  validates :last_name, uniqueness: true
+  validates :nickname, uniqueness: true
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
 end
